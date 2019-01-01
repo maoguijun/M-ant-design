571c819f5d9ab7c327b63cb67f39761854855c94
maoguijun <1053475583@qq.com>
2019-01-01 17:54:54 +0800

    deploy

---
+++ build/precache-manifest.7d17ca39886d56a6ce77328e44cfdd98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b7f2dfe7d1f4a1d1894",
+    "url": "/static/js/main.3b7f2dfe.chunk.js"
+  },
+  {
+    "revision": "88029c296a408f83898c",
+    "url": "/static/js/1.88029c29.chunk.js"
+  },
+  {
+    "revision": "3b7f2dfe7d1f4a1d1894",
+    "url": "/static/css/main.46d8f0a4.chunk.css"
+  },
+  {
+    "revision": "88029c296a408f83898c",
+    "url": "/static/css/1.8c7b839e.chunk.css"
+  },
+  {
+    "revision": "a3b582b78776328232953a0dd5da5f7e",
+    "url": "/index.html"
+  }
+];
